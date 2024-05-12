8cb05cac007a2a353801a0782fea097c5a283def
yummy moon <a191829064491@outlook.com>
2024-05-12 20:48:15 +0800

    main

---
+++ logging.java
@@ -0,0 +1,14 @@
+abstract class Curve {
+    int num;
+    String curname;
+    float depth[N];
+    float curvedata[N];
+    abstract public void processing();
+
+}
+
+public class logging {
+    public static void main(String[] args) {
+        
+    }
+}
