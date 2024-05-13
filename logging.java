1ba9ff63d67318d4e6c154e62c56d32d10045e6d
yummy moon <a191829064491@outlook.com>
2024-05-13 08:35:35 +0800

    main

--- logging.java
+++ logging.java
@@ -1,8 +1,18 @@
+//菜单
 abstract class Curve {
     int num;
     String curname;
-    float depth[N];
-    float curvedata[N];
+    static final int N = 15; 
+    float[] depth;
+    float[] curvedata;
+    
+    // 初始化depth和curvedata数组
+    public Curve(int num) {
+        this.num = num;
+        this.depth = new float[num];
+        this.curvedata = new float[num];
+    }
+
     abstract public void processing();
 
 }
